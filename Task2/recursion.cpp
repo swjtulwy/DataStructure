c357b8b5f6e668b8d542b8585ef133073191b9ba
lwy <swjtulauweiyee@gmail.com>
2019-08-09 13:11:03 +0800

    基本完成Task2

---
+++ Task2/recursion.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 本文件中函数全部采用递归实现
+
+//编程实现斐波那契数列求值 f(n) = f(n-1) + f(n-2)
+int Fibonacci(int n) {
+	if (n < 1) {
+		cout << "number n must greater than 0!" << endl;
+		exit(1);
+	}
+	if (n == 1 || n == 2) {
+		return 1; 
+	}
+	return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+
+//编程实现求阶乘 n!
+int Factorial(int n) {
+	if (n < 0) {
+		cout << "number n must greater than or equal to 0!" << endl;
+		exit(1);
+	}
+	if (n == 0) {
+		return 1;
+	}
+	return n * Factorial(n - 1);
+}
+
+//编程实现一组数据集合的全排列
+void FullPermutation(vector<int> v) {
+	
+}
+
+// 测试函数
+extern void TestFibonacci() {
+	cout << "Fibonacci sequence from 1 to 9: ";
+	for (int i = 1; i < 10; i++) {
+		cout << Fibonacci(i) << " ";
+	}
+	cout << endl;
+}
+
+extern void TestFactorial() {
+	cout << "Factorial sequence from 0 to 9: ";
+	for (int i = 0; i < 10; i++) {
+		cout << Factorial(i) << " ";
+	}
+	cout << endl;
+}
+
+extern void TestFullPermutation() {
+	
+}
--- Task2/test_main.cpp
+++ Task2/test_main.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include "stack_array.h"
 #include "stack_linked_list.h"
 #include "queue_array.h"
@@ -5,10 +6,17 @@
 #include "queue_circle.h"
 
 int main() {
+	extern void TestFibonacci();
+	extern void TestFactorial();
+	extern void TestFullPermutation();
+
 	//TestStackArray();			// 测试数组实现的栈
 	//TestStackLinkedList();	// 测试链表实现的栈
 	//TestQueueArray();			// 测试数组实现的顺序队列
 	//TestQueneLinkedList();	// 测试链表实现的队列
-	TestQueueCircle();          // 测试数组实现的循环队列
+	//TestQueueCircle();        // 测试数组实现的循环队列
+	TestFibonacci();			// 测试斐波那契数列
+	TestFactorial();		    // 测试阶乘
+	TestFullPermutation();		// 测试全排列
 	return 0;
 }
