0f24b9bd52cac8c02a180f1d42c95f90ed3e43fd
lwy <swjtulauweiyee@gmail.com>
2019-08-09 20:52:04 +0800

    fix bugs

--- Task2/recursion.cpp
+++ Task2/recursion.cpp
@@ -33,7 +33,36 @@ int Factorial(int n) {
 //编程实现一组数据集合的全排列
 // 包含begin 不包含end,[begin,end)
 void FullPermutation(vector<int> v, int begin, int end) {
-	
+	//if (begin == end) {
+	//	cout << endl;
+	//}
+	//else
+	//{
+	//	for (int i = begin; i < end; i++) {
+	//		swap(v[begin], v[i]);
+	//		cout << v[begin] << " ";
+	//		FullPermutation(v, begin + 1, end);
+	//		swap(v[i], v[begin]);
+	//	}
+	//}
+
+	//begin=end时，表示递归结束，找到了一个排列组合
+	if (begin == end)
+	{
+		for (int i = 0; i < end; i++)
+		{
+			cout << v[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+	for (int i = begin; i < end; i++)
+	{
+		swap(v[begin], v[i]);
+		FullPermutation(v,begin + 1, end);
+		swap(v[i], v[begin]);//回溯,把顺序还原
+	}
+
 }
 
 // 测试函数
