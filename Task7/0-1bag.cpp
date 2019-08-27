8a603de0cd25b44f67703275aa72e3fc113e9799
lwy <swjtulauweiyee@gmail.com>
2019-08-27 16:01:21 +0800

    fix bugs

--- Task7/0-1bag.cpp
+++ Task7/0-1bag.cpp
@@ -13,50 +13,60 @@ using namespace std;
 
 // https://blog.csdn.net/u010323330/article/details/80302227
 
-int maxv = -9999;
-int n = 3; // 物品数量
-int wMax = 30; // 背包容量
-vector<int> w{0, 16,15,15};  // 存放物品重量的数组
-vector<int> v{0,45,25,25};  // 存放物品价值的数组
+int bestv = -1;  // 最优价值
+int numObject = 5; // 物品数量
+int wMax = 10; // 背包容量
+vector<int> w{2,5,6,2,4};  // 存放物品重量的数组
+vector<int> v{6,4,5,3,6};  // 存放物品价值的数组
 
-int cw; // 当前重量
-int cv; // 当前价值
-vector<int> put(n+1);
-int total = 1; // 解空间结点数量
+int totalM = 0; // 可能的选法个数
+int bestid = 0;
+vector<int> bestx(numObject);  // 存储最好的选法
 
 
-void DFS_Back(int i, int cw,int cv, int rw, vector<int> op) {
-	// i为树第几层, cw为当前物品总重量，cv为当前总价值,rw为剩余物品总重量,op为存储数组
+void Test01Bag() {
+	vector<int> put(numObject);
+	int rw = 0;
+	for (int i = 0; i < numObject; i++) {
+		rw += w[i];  // 初始化剩余物品总价值
+	}
+	DFS_Back(1, 0, 0, rw, put);
+	cout << "选择方案共有上述" << totalM << "种" << endl;
+	cout << "其中第"<<bestid<<"方案具有最大价值" << bestv <<endl;
+	cout << "该方案为:";
+	for (int j = 0; j < numObject; j++) {
+		cout << bestx[j] << " ";
+	}
+	cout << endl;
+}
+
+void DFS_Back(int i, int cw, int cv, int rw, vector<int>& put) {
+	// i 为树第几层, cw(current weight) 为当前物品总重量，cv(current value) 为当前总价值, 
+	// rw (reserve weight) 为剩余物品总重量,op(option) 为选择数组
 	// 递归结束条件
-	if (i > n) { // 找到一个叶子结点
-		if (cw == wMax && cv > maxv) { // 找到一个满足条件的更优解
-			maxv = cv;
-			for (int j = 1; j <= n; j++) {
-				put[j] = op[j];
+	if (i > numObject) { // 找到一个叶子结点
+		for (int j = 0; j < numObject; j++) {
+			cout << put[j] << " ";
+		}
+		cout << endl;
+		++totalM;
+		if (cw <= wMax && cv > bestv) { // 找到一个满足条件的更优解
+			bestv = cv;
+			bestid = totalM;
+			for (int j = 0; j < numObject; j++) {
+				bestx[j] = put[j];
 			}
 		}
 	}
-	else{ // 尚未找完所有物品
-		if (cw + w[i] < wMax) { // 左孩子结点剪枝
-			op[i] = 1;   // 选第i个物品
-			DFS_Back(i + 1, cw + w[i], cv + v[i], rw - w[i], op);
+	else { // 尚未找完所有物品
+		if (cw + w[i - 1] <= wMax) { // 左孩子
+			put[i - 1] = 1;   // 选第i个物品
+			DFS_Back(i + 1, cw + w[i - 1], cv + v[i - 1], rw - w[i - 1], put);
 		}
-		op[i] = 0;  // 回溯，不选第i个物品
-		// 如果剩下能容纳的重量小于剩下的物品重量和，剪枝右边
-		if (wMax - cw < rw) {  
-			DFS_Back(i + 1, cw, cv, rw - w[i], op);
-		}
-	}
-}
-
-void Test01Bag() {
-	vector<int> op(n + 1);
-	int rw = 0;
-	for (int i = 0; i < n; i++) {
-		rw += w[i];  // 初始化剩余物品总价值
-	}
-	DFS_Back(1, 0, 0, rw, op);
-	for (int i = 0; i < n + 1; i++) {
-		cout << put[i] << " ";
+		put[i - 1] = 0;  // 回溯，不选第i个物品
+		// 如果剩下能容纳的重量小于剩下的物品重量和，右孩子
+		//if (wMax - cw < rw) {  // 可选择剪枝
+			DFS_Back(i + 1, cw, cv, rw - w[i - 1], put);
+		//}
 	}
 }
--- Task7/0-1bag.h
+++ Task7/0-1bag.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <vector>
 
 
+void DFS_Back(int i, int cw, int cv, int rw, std::vector<int>& op);
 void Test01Bag();
